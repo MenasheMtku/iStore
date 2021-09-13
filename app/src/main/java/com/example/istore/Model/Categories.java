c221c4f7b4fa86b2b3f1ef609af1709fb883b426
mmatu <mmatuko1@gmail.com>
2021-09-13 22:46:53 +0300

    Image Added on Viewstock.java

---
+++ app/src/main/java/com/example/istore/Model/Categories.java
@@ -0,0 +1,14 @@
+package com.example.istore.Model;
+
+public class Categories {
+
+    public static final String [] productCategories = {
+
+            "Food",
+            "Beverages",
+            "Chocolates",
+            "Electronics",
+            "Pharmacy",
+            "Other"
+    };
+}
